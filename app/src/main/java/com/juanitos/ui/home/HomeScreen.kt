de2eebf1a01ab45eed6936cd0ddfaacfca880804
Juan Martín Olivas <juanmarolivas@gmail.com>
2025-04-26 21:34:59 +0200

    add some icons, add cards for different sub apps in home

--- app/src/main/java/com/juanitos/ui/home/HomeScreen.kt
+++ app/src/main/java/com/juanitos/ui/home/HomeScreen.kt
@@ -1,15 +1,28 @@
 package com.juanitos.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.juanitos.R
 import com.juanitos.ui.navigation.NavigationDestination
 import com.juanitos.ui.navigation.JuanitOSTopAppBar
@@ -23,18 +36,51 @@ object HomeDestination : NavigationDestination {
 @Composable
 fun HomeScreen() {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold(
-        topBar = {
-            JuanitOSTopAppBar(
-                title = stringResource(HomeDestination.titleRes),
-                canNavigateBack = false,
-                scrollBehavior = scrollBehavior,
-            )
+    Scaffold(topBar = {
+        JuanitOSTopAppBar(
+            title = stringResource(HomeDestination.titleRes),
+            canNavigateBack = false,
+            scrollBehavior = scrollBehavior,
+        )
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                SubAppCard(textId = R.string.food, iconId = R.drawable.subapp_food, onClick = { })
+                SubAppCard(textId = R.string.money, iconId = R.drawable.subapp_money, onClick = { })
+            }
         }
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("hola")
-            Text("adios")
+    }
+}
+
+@Composable
+fun SubAppCard(textId: Int, iconId: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .size(128.dp)
+            .padding(dimensionResource(id = R.dimen.padding_small)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            IconButton(onClick = onClick, modifier = Modifier.fillMaxSize()) {
+                Icon(
+                    painter = painterResource(iconId),
+                    contentDescription = stringResource(textId),
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
