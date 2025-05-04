18ca0b64a48c8e147b0882936b4c24666b35c5a4
Juan Martín Olivas <juanmarolivas@gmail.com>
2025-05-04 21:11:23 +0200

    cleanup

--- app/src/main/java/com/juanitos/ui/food/NewBatchFoodScreen.kt
+++ app/src/main/java/com/juanitos/ui/food/NewBatchFoodScreen.kt
@@ -2,17 +2,14 @@ package com.juanitos.ui.food
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
