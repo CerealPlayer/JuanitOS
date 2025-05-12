0167e376b63a6db073c4fd7e1379b4687adc7127
Juan Martín Olivas <juanmarolivas@gmail.com>
2025-05-12 21:32:13 +0200

    avoid spacing issues in qt dialog

--- app/src/main/java/com/juanitos/ui/commons/QtDialog.kt
+++ app/src/main/java/com/juanitos/ui/commons/QtDialog.kt
@@ -2,10 +2,8 @@ package com.juanitos.ui.commons
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -13,7 +11,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -39,24 +36,16 @@ fun IngredientQtDialog(
                     .fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_medium)),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = name, style = MaterialTheme.typography.titleMedium)
-                    OutlinedTextField(
-                        value = qt,
-                        onValueChange = onQtChange,
-                        label = { Text(stringResource(R.string.qt)) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        modifier = Modifier.width(dimensionResource(R.dimen.input_width)),
-                        isError = isError,
-                        singleLine = true,
-                    )
-                }
+                Text(text = name, style = MaterialTheme.typography.titleMedium)
+                OutlinedTextField(
+                    value = qt,
+                    onValueChange = onQtChange,
+                    label = { Text(stringResource(R.string.qt)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.fillMaxWidth(),
+                    isError = isError,
+                    singleLine = true,
+                )
                 customMessage()
                 Button(
                     onClick = onSave, modifier = Modifier.fillMaxWidth()
