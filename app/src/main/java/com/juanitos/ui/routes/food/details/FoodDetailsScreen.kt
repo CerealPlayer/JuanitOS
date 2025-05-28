8cbd15e47ee96ee9e87be68b84db5aae3d01e0da
Juan Martín Olivas <juanmarolivas@gmail.com>
2025-05-28 19:40:16 +0200

    add menu to food details

--- app/src/main/java/com/juanitos/ui/routes/food/details/FoodDetailsScreen.kt
+++ app/src/main/java/com/juanitos/ui/routes/food/details/FoodDetailsScreen.kt
@@ -2,11 +2,14 @@ package com.juanitos.ui.routes.food.details
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,6 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -51,12 +58,10 @@ fun FoodDetailsScreen(
             navigateUp = onNavigateUp,
             canNavigateBack = true,
             actions = {
-                IconButton(onClick = { viewModel.deleteFood(onNavigateUp) }) {
-                    Icon(
-                        imageVector = Icons.Filled.Delete,
-                        contentDescription = stringResource(R.string.delete),
-                    )
-                }
+                OptionsMenu(
+                    onDelete = {},
+                    onEdit = {}
+                )
             })
     }) { paddingValues ->
         Column(
@@ -105,5 +110,35 @@ fun FoodIngredientCard(foodIngredient: FoodIngredientDetails) {
                 batchFood = batchFoodIngredient.toBatchFoodWithIngredientDetails()
             )
         )
+    }
+}
+
+@Composable
+fun OptionsMenu(
+    onDelete: () -> Unit,
+    onEdit: () -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))) {
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = stringResource(R.string.settings),
+            )
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.edit)) }, onClick = {
+                    onEdit()
+                    expanded = false
+                }
+            )
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.delete)) }, onClick = {
+                    onDelete()
+                    expanded = false
+                }
+            )
+        }
     }
 }
