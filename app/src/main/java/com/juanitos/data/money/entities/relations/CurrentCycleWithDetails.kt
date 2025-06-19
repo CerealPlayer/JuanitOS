a2ace39b371effc49e06824dbccbab2e70d46a06
Juan Martín Olivas <juanmarolivas@gmail.com>
2025-06-19 13:51:05 +0200

    fix relation class

--- app/src/main/java/com/juanitos/data/money/entities/relations/CurrentCycleWithDetails.kt
+++ app/src/main/java/com/juanitos/data/money/entities/relations/CurrentCycleWithDetails.kt
@@ -1,11 +1,24 @@
 package com.juanitos.data.money.entities.relations
 
+import androidx.room.Embedded
+import androidx.room.Relation
 import com.juanitos.data.money.entities.Cycle
 import com.juanitos.data.money.entities.FixedSpending
 import com.juanitos.data.money.entities.Transaction
 
 data class CurrentCycleWithDetails(
+    @Embedded
     val cycle: Cycle,
+    @Relation(
+        entity = Transaction::class,
+        parentColumn = "id",
+        entityColumn = "cycle_id"
+    )
     val transactions: List<Transaction>,
+    @Relation(
+        entity = FixedSpending::class,
+        parentColumn = "id",
+        entityColumn = "cycle_id"
+    )
     val fixedSpendings: List<FixedSpending>
 )
