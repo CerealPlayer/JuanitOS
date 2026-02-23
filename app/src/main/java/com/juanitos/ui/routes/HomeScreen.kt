070a060f436a4f84d487ed1f4ba63ff9c4a4b25b
Juan Martín Olivas <juanmarolivas@gmail.com>
2026-02-23 11:41:03 +0100

    add BottomAppBar to HomeScreen for navigation to MoneyScreen

--- app/src/main/java/com/juanitos/ui/routes/HomeScreen.kt
+++ app/src/main/java/com/juanitos/ui/routes/HomeScreen.kt
@@ -1,27 +1,18 @@
 package com.juanitos.ui.routes
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.juanitos.R
 import com.juanitos.ui.navigation.JuanitOSTopAppBar
 import com.juanitos.ui.navigation.NavigationDestination
@@ -43,6 +34,15 @@ fun HomeScreen(
             title = stringResource(HomeDestination.titleRes),
             canNavigateBack = false,
         )
+    }, bottomBar = {
+        BottomAppBar(actions = {
+            IconButton(onClick = { onNavigateTo(MoneyDestination.route) }) {
+                Icon(
+                    painter = painterResource(R.drawable.subapp_money),
+                    contentDescription = stringResource(R.string.money)
+                )
+            }
+        })
     }) { innerPadding ->
         Column(
             modifier = Modifier
@@ -50,48 +50,7 @@ fun HomeScreen(
                 .fillMaxHeight(),
             verticalArrangement = Arrangement.Center
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                SubAppCard(textId = R.string.money, iconId = R.drawable.subapp_money, onClick = {
-                    onNavigateTo(
-                        MoneyDestination.route
-                    )
-                })
-            }
+
         }
     }
-}
-
-@Composable
-fun SubAppCard(textId: Int, iconId: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier
-            .size(128.dp)
-            .padding(dimensionResource(id = R.dimen.padding_small))
-            .clickable(
-                onClick = onClick
-            ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Icon(
-                painter = painterResource(iconId),
-                contentDescription = stringResource(textId),
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-fun HomePreview() {
-    HomeScreen(onNavigateTo = {})
 }
